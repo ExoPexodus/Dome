ae3724400ebf44921eff530f138eae84072a2c82
Rushil <rushilrana@gmail.com>
2023-04-06 20:10:40 +0530

    commit 4

--- Login_screen.py
+++ Login_screen.py
@@ -1,26 +1,30 @@
-import tkinter
-import tkinter.messagebox
-import customtkinter
-import psycopg2
-import psycopg2.extras
-import custom_functions
+import tkinter  # for the basic GUI features
+import tkinter.messagebox   # For the default errors and message popups
+import customtkinter     # Custom Tkinter library for a cool Tkinter look
+import custom_functions     # Custom function python script for additional database connection related functions
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 #=============Global Variables===============
-
 username = None
 email = None
 password = None
 confirm_password = None
 #=========================DB Variables=====================
 cur, conn = custom_functions.connect()
-#=======================
+#=======================initialising the GUI====================
 app = customtkinter.CTk()
 app.title("Dome")
 app.geometry("450x200")
-#=======================
+#========================Function Decleration=========================
+
+# function to switch to register frame
+def register():
+    print("Account creation screen opened")
+    login.pack_forget()
+    reg.pack()
+#function to switch to welcome screen showing the username of the user
 def welcome():
     app.destroy()
     welcome_screen = customtkinter.CTk()
@@ -31,19 +35,12 @@ def welcome():
     welcome_label.pack(pady=5)
     welcome_screen.after(10,welcome_screen.destroy())
     welcome_screen.mainloop()
-#========================Function Decleration=========================
 
-def register():
-    print("Account creation screen opened")
-    login.pack_forget()
-    reg.pack()
-
-
+#function to go back to the login screen
 def back():
     print("Going back to login screen")
     reg.pack_forget()
     login.pack()
-
 
 def authentication():
     global name
@@ -82,21 +79,25 @@ def account_creation():
         check_user_script = "select * from users where name = '%s'"%(username)
         check_email_script = "select * from users where email = '%s'"%(email)
         if len(username) == 0:
-            tkinter.messagebox.showinfo("Error", "Error: username is empty")
+            tkinter.messagebox.showerror("Error", "Error: username is empty")
         else:
             cur.execute(check_user_script)
             if cur.fetchall():
-                tkinter.messagebox.showinfo("Error", "Error: username already exists")
+                tkinter.messagebox.showerror("Error", "Error: username already exists")
             else:
                 if len(email) == 0:
-                    tkinter.messagebox.showinfo("Error", "Error: Email is empty")
+                    tkinter.messagebox.showerror("Error", "Error: Email is empty")
+                if '@' not in email or not email.index('@') > 0 and email.index('@') < len(email) - 5:
+                    tkinter.messagebox.showerror("Error","Error: Email written incorrectly, please make sure you're writing the email correctly")
+                elif not email.endswith('.com'):
+                    tkinter.messagebox.showerror("Error","Error: Email written incorrectly, please make sure you're writing the email correctly")
                 else:
                     cur.execute(check_email_script)
                     if cur.fetchall():
-                        tkinter.messagebox.showinfo("Error", "Error: email already exists")
+                        tkinter.messagebox.showerror("Error", "Error: email already exists")
                     else:
                         if len(password) <= 7:
-                            tkinter.messagebox.showinfo("Error", "Error: Please make the password at least 8 characters for security reasons")
+                            tkinter.messagebox.showerror("Error", "Error: Please make the password at least 8 characters for security reasons")
                         else:
                             if password == confirm_password:
 
@@ -105,9 +106,14 @@ def account_creation():
                                 conn.commit()
                                 print("account created")
                                 tkinter.messagebox.showinfo("Message", "Account Successfully Created")
+                                back()
                             else:
                                 print("The passwords do not match")
-                                tkinter.messagebox.showinfo("Error", "Passwords do not match")
+                                tkinter.messagebox.showerror("Error", "Passwords do not match")
+
+def on_enter_press(event):
+    # simulate a button click
+    authentication()
 
 #=============================Login Frame==================================
 login = customtkinter.CTkFrame(app)
@@ -156,7 +162,7 @@ button_create_acc.grid(column=1,row=5,sticky= "s")
 button_back = customtkinter.CTkButton(reg, text="Go back", command=back)
 button_back.grid(column=2,row=5,sticky= "s")
 
-
+app.bind('<Return>', on_enter_press)
 login.pack()
 custom_functions.disconnect()
 app.mainloop()
